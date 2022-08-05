a81a683c1b8521c0cb4769e02243cf171a8acc65
gabrlaur <45432522+gabrlaur@users.noreply.github.com>
2022-08-05 18:38:01 +0300

    Task #4 fix

--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,10 +2,10 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
-        int c = 'c';
-        int h = 'h';
-        int a = 'a';
-        int r = 'r';
+        int c = 99;
+        int h = 104;
+        int a = 97;
+        int r = 114;
         System.out.println(c);
         System.out.println(h);
         System.out.println(a);
